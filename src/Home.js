d20a2d9ecd88d27d762a129330584a5bf1713944
Gilles Ferrand <gilles.ferrand31@gmail.com>
2020-12-29 21:50:46 +0100

    Add click events

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,19 @@
 const Home = () => {
     const title = 'Home page';
+
+    const handleClick = (e) => {
+        console.log('Hello World!', e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('Hello ' + name, e.target);
+    }
+
     return ( 
         <div className="home">
             <h2>{ title }</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain('Gilles', e)}>Click me again</button>
         </div>
      );
 }
